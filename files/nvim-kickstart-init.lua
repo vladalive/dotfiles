071c1db15a8a9b08ea91d06879d5ccc72b5770f9
Vlad Alive <vladalive@gmail.com>
2024-03-10 22:00:40 +0400

    [fix] nvim-kickstart: my config

--- files/nvim-kickstart-init.lua
+++ files/nvim-kickstart-init.lua
@@ -87,8 +87,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = '\\'
+vim.g.maplocalleader = '\\'
+
+vim.keymap.set('n', ';', ':')
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
@@ -105,7 +107,7 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = ''
 
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
@@ -149,14 +151,14 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 5
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '//', '<cmd>nohlsearch<CR>', { silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -186,6 +188,50 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- [[ My custom bindings ]]
+
+-- [[ My custom functions ]]
+
+--- NOTE: https://github.com/Shopify/ruby-lsp/blob/main/EDITORS.md#neovim-lsp
+-- textDocument/diagnostic support until 0.10.0 is released
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require('vim.lsp.diagnostic')._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request('textDocument/diagnostic', { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format('diagnostics error - %s', vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(nil, vim.tbl_extend('keep', params, { diagnostics = diagnostic_items }), { client_id = client.id })
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -372,7 +418,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>s\\', builtin.buffers, { desc = '[S][\\] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -570,6 +616,12 @@ require('lazy').setup({
               -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
+        },
+
+        ruby_ls = {
+          on_attach = function(client, buffer)
+            setup_diagnostics(client, buffer)
+          end,
         },
       }
 
@@ -785,7 +837,40 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = {
+          'vim',
+          'vimdoc',
+          'bash',
+          'c',
+          'comment',
+          'css',
+          'diff',
+          'dot',
+          'git_config',
+          'git_rebase',
+          'gitattributes',
+          'gitcommit',
+          'gitignore',
+          'html',
+          'javascript',
+          'jq',
+          'json',
+          'lua',
+          'markdown',
+          'markdown_inline',
+          'pug',
+          'python',
+          'regex',
+          'ruby',
+          'scss',
+          'sql',
+          'ssh_config',
+          'typescript',
+          'vim',
+          'vue',
+          'xml',
+          'yaml',
+        },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
@@ -819,6 +904,56 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
+
+  -- NOTE: My custom plugins
+
+  -- Alternate files
+  {
+    'tpope/vim-projectionist',
+    config = function()
+      vim.keymap.set('n', '<leader>a', ':A<CR>', { silent = true, desc = 'Go to [A]lternate file' })
+      vim.g.projectionist_heuristics = {
+        ['*'] = {
+          ['app/*.rb'] = {
+            type = 'specs',
+            alternate = 'spec/{}_spec.rb',
+          },
+          ['spec/*_spec.rb'] = {
+            type = 'specs',
+            alternate = 'app/{}.rb',
+          },
+        },
+      }
+    end,
+  },
+
+  -- Git fugitive
+  {
+    'tpope/vim-fugitive',
+  },
+
+  -- Zoom
+  {
+    'nyngwang/NeoZoom.lua',
+    enabled = true,
+    config = function()
+      vim.keymap.set('n', '<leader><leader>', function()
+        vim.cmd 'NeoZoomToggle'
+      end, { silent = true, nowait = true })
+      require('neo-zoom').setup {
+        popup = { enabled = true },
+        winopts = {
+          offset = {
+            top = 0,
+            left = 0,
+            width = 300,
+            height = 100,
+          },
+          border = 'none',
+        },
+      }
+    end,
+  },
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
