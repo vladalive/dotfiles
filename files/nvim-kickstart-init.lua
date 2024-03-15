c1a4836f97c68b03bda2a94f76f8c9e96f7bb0f3
Vlad Alive <vladalive@gmail.com>
2024-03-15 19:34:43 +0400

    [add] nvim: chatgpt

--- files/nvim-kickstart-init.lua
+++ files/nvim-kickstart-init.lua
@@ -1087,6 +1087,23 @@ require('lazy').setup({
     end
   },
 
+  -- ChatGPT
+  {
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'folke/trouble.nvim',
+      'nvim-telescope/telescope.nvim'
+    },
+    config = function()
+      require('chatgpt').setup {
+       api_key_cmd = 'cat /home/deployer/.config/.chatgpt.key',
+      }
+    end,
+  },
+
   -- Tree
   {
     'nvim-neo-tree/neo-tree.nvim',
