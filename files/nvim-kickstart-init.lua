0a6777f8a9348f056b80d09c44883023c646b2fe
Vlad Alive <vladalive@gmail.com>
2024-06-06 10:26:31 +0400

    [fix] nvim: disable autocomplete by enter

--- files/nvim-kickstart-init.lua
+++ files/nvim-kickstart-init.lua
@@ -798,9 +798,9 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-y>'] = cmp.config.disable,
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          -- ['<C-y>'] = cmp.config.disable,
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
