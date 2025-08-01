e80e1ed988ba6ba18d2db3b60edbbb7599f383f5
Vlad Alive <vladalive@gmail.com>
2025-08-01 13:07:02 +0400

    [add] nvim: avante: openrouter

--- files/nvim_custom_plugins/avante.lua
+++ files/nvim_custom_plugins/avante.lua
@@ -14,5 +14,25 @@ return {
     },
   },
   build = "make",
-  opts = { provider = "copilot" },
+  opts = {
+    -- debug = true,
+    provider = "copilot",
+    -- provider = "openrouter",
+    providers = {
+      copilot = {
+        extra_request_body = {
+          max_tokens = 50000,
+        },
+      },
+      openrouter = {
+        __inherited_from = 'openai',
+        endpoint = 'https://openrouter.ai/api/v1',
+        api_key_name = 'OPEN_ROUTER_API_KEY',
+        -- api_key_name = 'cmd:bw get notes key-open-router-coding',
+        model = 'qwen/qwen3-coder:free',
+        -- model = 'deepseek/deepseek-chat-v3-0324:free',
+        timeout = 30000,
+      },
+    },
+  },
 }
