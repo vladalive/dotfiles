b52358aae3a60bc3db72ee5365e312d0461d280e
Vlad Alive <vladalive@gmail.com>
2025-08-07 12:27:38 +0400

    [refactor] nvim: extract configs

---
+++ files/nvim_custom_plugins/conform.lua
@@ -0,0 +1,29 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = {
+    'BufWritePre',
+    'BufReadPre',
+    'BufNewFile',
+  },
+  cmd = { 'ConformInfo' },
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      ruby = { 'rubocop' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use a sub-list to tell conform to run *until* a formatter
+      -- is found.
+      -- javascript = { { "prettierd", "prettier" } },
+    },
+  },
+}
