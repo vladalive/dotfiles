f0394de715317649b1bb182e9e449ed288382cde
Vlad Alive <vladalive@gmail.com>
2026-03-22 13:43:36 +0400

    [fix] nvim: disable other autocompletions

--- files/nvim_custom_plugins/nvim_cmp.lua
+++ files/nvim_custom_plugins/nvim_cmp.lua
@@ -121,9 +121,9 @@ return { -- Autocompletion
         }),
       },
       sources = {
-        { name = 'cmp_tabnine', group_index = 2 },
+        -- { name = 'cmp_tabnine', group_index = 2 },
         { name = 'copilot', group_index = 2 },
-        { name = 'codeium', group_index = 2 },
+        -- { name = 'codeium', group_index = 2 },
         { name = 'nvim_lsp', group_index = 2 },
         { name = 'path', group_index = 2 },
         { name = 'luasnip', group_index = 2 },
