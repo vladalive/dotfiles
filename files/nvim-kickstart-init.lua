d36bf58451bfee917916288d4f4804a8852fba5c
Vlad Alive <vladalive@gmail.com>
2026-05-19 01:27:10 +0400

    refactor: extract neovim autocommands

--- files/nvim-kickstart-init.lua
+++ files/nvim-kickstart-init.lua
@@ -8,20 +8,7 @@ end
 load_core 'options'
 load_core 'commands'
 load_core 'keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+load_core 'autocmds'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
