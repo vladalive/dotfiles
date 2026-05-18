782c4beacd1e502107f95976663d176351ffb3dd
Vlad Alive <vladalive@gmail.com>
2026-05-19 01:26:36 +0400

    refactor: extract neovim commands

---
+++ files/nvim_core/commands.lua
@@ -0,0 +1,41 @@
+vim.cmd [[
+  function! RunSubstitutions()
+    execute "'<,'>s/\\s*$//g"
+    execute "'<,'>s/\"/'/g"
+    execute "'<,'>s/:\\([^= ]*\\)\\s*=>\\s*/\\1: /g"
+    execute "'<,'>s/{\\([^ ].*[^ ]\\)}/{ \\1 }/g"
+    execute "'<,'>s/^\\(\\s*\\)+/\\1/g"
+    execute "'<,'>s/\\[/ /g"
+    execute "'<,'>s/\\]/,/g"
+  endfunction
+]]
+
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
+vim.api.nvim_create_user_command('Format', function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ['end'] = { args.line2, end_line:len() },
+    }
+  end
+  require('conform').format({ async = true, lsp_format = 'fallback', range = range })
+end, { range = true })
