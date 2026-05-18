9396e7a4b70d254adb0cbdac2c0c86c74d94586d
Vlad Alive <vladalive@gmail.com>
2026-05-19 03:37:48 +0400

    fix: ensure neo-tree preview target window

--- files/nvim_custom_plugins/neo_tree.lua
+++ files/nvim_custom_plugins/neo_tree.lua
@@ -8,17 +8,35 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
+    local preview_config = {
+      use_float = false,
+      use_snacks_image = true,
+      use_image_nvim = true,
+    }
+
+    local function preview(state)
+      local utils = require('neo-tree.utils')
+      local _, is_neo_tree_window = utils.get_appropriate_window(state)
+
+      if is_neo_tree_window then
+        local tree_win = vim.api.nvim_get_current_win()
+        local split = state.current_position == 'right' and 'leftabove vnew' or 'rightbelow vnew'
+
+        vim.cmd(split)
+        vim.bo.buftype = 'nofile'
+        vim.bo.bufhidden = 'wipe'
+        vim.bo.swapfile = false
+        vim.api.nvim_set_current_win(tree_win)
+      end
+
+      state.config = preview_config
+      state.commands.preview(state)
+    end
+
     require('neo-tree').setup {
       window = {
         mappings = {
-          ['P'] = {
-            'preview',
-            config = {
-              use_float = false,
-              use_snacks_image = true,
-              use_image_nvim = true,
-            },
-          },
+          ['P'] = preview,
         },
       },
       filesystem = {
