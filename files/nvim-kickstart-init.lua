55aba87f607644b12b4ca62731dea1e878e99efd
Vlad Alive <vladalive@gmail.com>
2026-05-19 01:12:05 +0400

    refactor: move todo comments plugin spec

--- files/nvim-kickstart-init.lua
+++ files/nvim-kickstart-init.lua
@@ -307,9 +307,6 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
