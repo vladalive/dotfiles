d6208b8ab37a8c50205b71b97c591d9e1d26c0a7
Vlad Alive <vladalive@gmail.com>
2026-05-19 02:15:52 +0400

    fix: avoid copilot cmp lsp deprecation warning

--- files/nvim_custom_plugins/copilot_cmp.lua
+++ files/nvim_custom_plugins/copilot_cmp.lua
@@ -2,6 +2,30 @@
 return {
   'zbirenbaum/copilot-cmp',
   config = function ()
+    local source = require('copilot_cmp.source')
+
+    function source:is_available()
+      if self.client:is_stopped() or self.client.name ~= 'copilot' then
+        return false
+      end
+
+      local get_source_client = function()
+        if vim.lsp.get_clients == nil then
+          return vim.lsp.get_active_clients({
+            bufnr = vim.api.nvim_get_current_buf(),
+            id = self.client.id,
+          })
+        end
+
+        return vim.lsp.get_clients({
+          bufnr = vim.api.nvim_get_current_buf(),
+          id = self.client.id,
+        })
+      end
+
+      return next(get_source_client()) ~= nil
+    end
+
     require('copilot_cmp').setup()
   end
 }
