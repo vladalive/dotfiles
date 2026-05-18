48637b8eac7c9678c94e39dbe2ee0b7fd146317f
Vlad Alive <vladalive@gmail.com>
2026-05-19 03:28:34 +0400

    fix: avoid floating neo-tree preview errors

--- files/nvim_custom_plugins/neo_tree.lua
+++ files/nvim_custom_plugins/neo_tree.lua
@@ -9,6 +9,18 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      window = {
+        mappings = {
+          ['P'] = {
+            'toggle_preview',
+            config = {
+              use_float = false,
+              use_snacks_image = true,
+              use_image_nvim = true,
+            },
+          },
+        },
+      },
       filesystem = {
         filtered_items = {
           hide_by_name = {
