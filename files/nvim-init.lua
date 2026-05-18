3f2bfed211fc179534f49c3839da232814aaec3b
Vlad Alive <vladalive@gmail.com>
2026-05-19 03:14:50 +0400

    refactor: remove stale neovim diagnostic suppression

--- files/nvim-init.lua
+++ files/nvim-init.lua
@@ -14,7 +14,7 @@ local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
