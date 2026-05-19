aeb61d0e9ad63c80cb3362892a05109d72de7160
Vlad Alive <vladalive@gmail.com>
2026-05-19 21:58:51 +0400

    chore: add which-key leader groups

--- files/nvim_custom_plugins/which_key.lua
+++ files/nvim_custom_plugins/which_key.lua
@@ -1,21 +1,15 @@
--- NOTE: Plugins can also be configured to run lua code when they are loaded.
---
--- This is often very useful to both group configuration, as well as handle
--- lazy loading plugins that don't need to be loaded immediately at startup.
---
--- For example, in the following configuration, we use:
---  event = 'VimEnter'
---
--- which loads which-key before all the UI elements are loaded. Events can be
--- normal autocommands events (`:help autocmd-events`).
---
--- Then, because we use the `config` key, the configuration only runs
--- after the plugin has been loaded:
---  config = function() ... end
-
-return { -- Useful plugin to show you pending keybinds.
+return {
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  event = 'VimEnter',
+  opts = {
+    spec = {
+      { '<leader>c', group = 'Code' },
+      { '<leader>cc', group = 'Copilot Chat' },
+      { '<leader>d', group = 'Document / Diagnostics' },
+      { '<leader>s', group = 'Search' },
+      { '<leader>w', group = 'Workspace' },
+    },
+  },
   keys = {
     {
       '<leader>?',
