16721416f7c8f2f618bca6664ad4c8c5ed0753d1
Vlad Alive <vladalive@gmail.com>
2026-05-20 20:54:36 +0400

    perf: lazy-load vim fugitive

--- files/nvim_custom_plugins/git_fugitive.lua
+++ files/nvim_custom_plugins/git_fugitive.lua
@@ -1,4 +1,23 @@
 -- Git fugitive
 return {
   'tpope/vim-fugitive',
+  cmd = {
+    'G',
+    'Git',
+    'Gcd',
+    'Gclog',
+    'Gdiffsplit',
+    'Gdrop',
+    'Gedit',
+    'Ggrep',
+    'Ghdiffsplit',
+    'Glcd',
+    'Gllog',
+    'Gread',
+    'Gsplit',
+    'Gtabedit',
+    'Gvdiffsplit',
+    'Gvsplit',
+    'Gwrite',
+  },
 }
