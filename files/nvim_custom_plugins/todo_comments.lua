5ecd8e034739e40d83d4c2ee882932e81de70116
Vlad Alive <vladalive@gmail.com>
2026-05-20 21:10:17 +0400

    perf: defer todo comments until file open

--- files/nvim_custom_plugins/todo_comments.lua
+++ files/nvim_custom_plugins/todo_comments.lua
@@ -1,6 +1,6 @@
 return {
   'folke/todo-comments.nvim',
-  event = 'VimEnter',
+  event = { 'BufReadPost', 'BufNewFile' },
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = { signs = false },
 }
